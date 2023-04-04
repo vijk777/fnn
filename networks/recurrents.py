399973c609cad7ecd6cd3338f130967f287acaa5
Eric Y. Wang <ericwang919@gmail.com>
2023-04-04 12:04:33 -0500

    recurrents

--- networks/recurrents.py
+++ networks/recurrents.py
@@ -24,20 +24,17 @@ class RvT(Recurrent):
         in_channels: Sequence[int],
         out_channels: int,
         kernel_size: int,
-        cells: int = 1,
-        ensembles: int = 1,
+        groups: int = 1,
     ):
         super().__init__(in_channels, out_channels)
 
         self.out_channels = int(out_channels)
         self.kernel_size = int(kernel_size)
-        self.cells = int(cells)
-        self.ensembles = int(ensembles)
-        self.heads = self.ensembles * self.cells
-        self.head_channels = self.out_channels // self.heads
+        self.groups = int(groups)
+        self.group_channels = self.out_channels // self.groups
 
-        self.tau = nn.Parameter(torch.ones(self.heads))
-        nn.init.constant_(self.tau, self.head_channels**-0.5)
+        self.tau = nn.Parameter(torch.ones(self.groups))
+        nn.init.constant_(self.tau, self.group_channels**-0.5)
 
         self.drop = Dropout(
             drop_dim=[4, 5],
@@ -46,7 +43,7 @@ class RvT(Recurrent):
 
         self.proj_x = Conv(
             out_channels=self.out_channels,
-            out_groups=self.heads,
+            out_groups=self.groups,
             gain=False,
             bias=False,
         )
@@ -54,73 +51,73 @@ class RvT(Recurrent):
         for c in self.in_channels:
             self.proj_x.add(in_channels=c)
 
-        if self.cells > 1:
-            self.proj_x.add_intercell(cells=self.cells)
+        if self.groups > 1:
+            self.proj_x.add_intergroup()
 
-        self.proj_q = Conv(out_channels=self.out_channels, out_groups=self.heads, gain=False, bias=False)
+        self.proj_q = Conv(out_channels=self.out_channels, out_groups=self.groups, gain=False, bias=False)
         self.proj_q.add(
             in_channels=self.out_channels * 2,
-            in_groups=self.heads,
+            in_groups=self.groups,
             kernel_size=self.kernel_size,
         )
 
-        self.proj_k = Conv(out_channels=self.out_channels, out_groups=self.heads, gain=False, bias=False)
+        self.proj_k = Conv(out_channels=self.out_channels, out_groups=self.groups, gain=False, bias=False)
         self.proj_k.add(
             in_channels=self.out_channels * 2,
-            in_groups=self.heads,
+            in_groups=self.groups,
             kernel_size=self.kernel_size,
             pad=False,
         )
 
-        self.proj_v = Conv(out_channels=self.out_channels, out_groups=self.heads, gain=False, bias=False)
+        self.proj_v = Conv(out_channels=self.out_channels, out_groups=self.groups, gain=False, bias=False)
         self.proj_v.add(
             in_channels=self.out_channels * 2,
-            in_groups=self.heads,
+            in_groups=self.groups,
             kernel_size=self.kernel_size,
             pad=False,
         )
 
-        self.proj_i = Conv(out_channels=self.out_channels, out_groups=self.heads)
+        self.proj_i = Conv(out_channels=self.out_channels, out_groups=self.groups)
         self.proj_i.add(
             in_channels=self.out_channels,
-            in_groups=self.heads,
+            in_groups=self.groups,
         )
         self.proj_i.add(
             in_channels=self.out_channels * 2,
-            in_groups=self.heads,
+            in_groups=self.groups,
             kernel_size=self.kernel_size,
         )
 
-        self.proj_f = Conv(out_channels=self.out_channels, out_groups=self.heads)
+        self.proj_f = Conv(out_channels=self.out_channels, out_groups=self.groups)
         self.proj_f.add(
             in_channels=self.out_channels,
-            in_groups=self.heads,
+            in_groups=self.groups,
         )
         self.proj_f.add(
             in_channels=self.out_channels * 2,
-            in_groups=self.heads,
+            in_groups=self.groups,
             kernel_size=self.kernel_size,
         )
 
-        self.proj_g = Conv(out_channels=self.out_channels, out_groups=self.heads)
+        self.proj_g = Conv(out_channels=self.out_channels, out_groups=self.groups)
         self.proj_g.add(
             in_channels=self.out_channels,
-            in_groups=self.heads,
+            in_groups=self.groups,
         )
         self.proj_g.add(
             in_channels=self.out_channels * 2,
-            in_groups=self.heads,
+            in_groups=self.groups,
             kernel_size=self.kernel_size,
         )
 
-        self.proj_o = Conv(out_channels=self.out_channels, out_groups=self.heads)
+        self.proj_o = Conv(out_channels=self.out_channels, out_groups=self.groups)
         self.proj_o.add(
             in_channels=self.out_channels,
-            in_groups=self.heads,
+            in_groups=self.groups,
         )
         self.proj_o.add(
             in_channels=self.out_channels * 2,
-            in_groups=self.heads,
+            in_groups=self.groups,
             kernel_size=self.kernel_size,
         )
 
@@ -147,17 +144,17 @@ class RvT(Recurrent):
         else:
             h = c = torch.zeros(1, self.out_channels, 1, 1, device=self.device)
 
-        if self.cells > 1:
+        if self.groups > 1:
             x = self.proj_x([*inputs, h])
         else:
             x = self.proj_x(inputs)
 
-        xh = torch.stack([to_groups_2d(x, self.heads), to_groups_2d(h.expand_as(x), self.heads)], 2)
+        xh = torch.stack([to_groups_2d(x, self.groups), to_groups_2d(h.expand_as(x), self.groups)], 2)
         xh = self.drop(xh, p=dropout).flatten(1, 3)
 
-        q = to_groups_2d(self.proj_q([xh]), self.heads).flatten(3, 4)
-        k = to_groups_2d(self.proj_k([xh]), self.heads).flatten(3, 4)
-        v = to_groups_2d(self.proj_v([xh]), self.heads).flatten(3, 4)
+        q = to_groups_2d(self.proj_q([xh]), self.groups).flatten(3, 4)
+        k = to_groups_2d(self.proj_k([xh]), self.groups).flatten(3, 4)
+        v = to_groups_2d(self.proj_v([xh]), self.groups).flatten(3, 4)
 
         w = torch.einsum("G, N G C Q , N G C D -> N G Q D", self.tau, q, k).softmax(dim=3)
         a = torch.einsum("N G C D , N G Q D -> N G C Q", v, w).view_as(x)
