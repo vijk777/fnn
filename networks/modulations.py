78242255b2d1b0572f98be88bcbcacebfeeba653
Eric Y. Wang <ericwang919@gmail.com>
2023-04-05 09:33:14 -0500

    modulations

---
+++ networks/modulations.py
@@ -0,0 +1,77 @@
+import torch
+from typing import Optional
+
+from .containers import Module
+from .standardization import Behavior
+from .elements import Linear
+
+
+class Modulation(Module):
+    def __init__(self, behavior: Behavior, out_channels: int):
+        super().__init__()
+        self.behavior = behavior
+        self.out_channels = int(out_channels)
+
+    def forward(self, behavior: Optional[torch.Tensor] = None):
+        """
+        Args:
+            behavior (torch.Tensor) : shape = [n, f]
+        Returns:
+            (torch.Tensor)          : shape = [n, c, 1, 1]
+        """
+        raise NotImplementedError()
+
+
+class LSTM(Modulation):
+    def __init__(
+        self,
+        behavior: Behavior,
+        size: int,
+    ):
+        super().__init__(
+            behavior=behavior,
+            out_channels=size,
+        )
+
+        linear = (
+            lambda: Linear(out_features=self.out_channels)
+            .add(in_features=self.behavior.n_features)
+            .add(in_features=self.out_channels)
+        )
+        self.proj_i = linear()
+        self.proj_f = linear()
+        self.proj_g = linear()
+        self.proj_o = linear()
+
+        self._past = dict()
+
+    def _reset(self):
+        self._past.clear()
+
+    def forward(self, behavior: Optional[torch.Tensor] = None):
+        """
+        Args:
+            behavior (torch.Tensor) : shape = [n, f]
+        Returns:
+            (torch.Tensor)          : shape = [n, c, 1, 1]
+        """
+        if self._past:
+            h = self._past["h"]
+            c = self._past["c"]
+        else:
+            h = c = torch.zeros(1, self.out_channels, device=self.device)
+
+        x = self.behavior(behavior)
+
+        i = torch.sigmoid(self.proj_i([x, h]))
+        f = torch.sigmoid(self.proj_f([x, h]))
+        g = torch.tanh(self.proj_g([x, h]))
+        o = torch.sigmoid(self.proj_o([x, h]))
+
+        c = f * c + i * g
+        h = o * torch.tanh(c)
+
+        self._past["c"] = c
+        self._past["h"] = h
+
+        return h[:, :, None, None]
