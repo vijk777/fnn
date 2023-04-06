34b8d93a2da0a7f92645652625c58f510a148dd7
Eric Y. Wang <ericwang919@gmail.com>
2023-04-06 12:43:33 -0500

    cortex

--- networks/retinas.py
+++ networks/retinas.py
@@ -1,39 +1,57 @@
 import torch
-from typing import Sequence, Optional
 
 from .containers import Module, ModuleList
 from .utils import isotropic_grid_2d
 
 
 class Retina(Module):
-    def grid(self, height: int = 144, width: int = 256):
+    def grid(self, height=144, width=256):
         """
-        Args:
-            height          (int)
-            width           (int)
-        Returns:
-            (torch.Tensor)                  : shape = [h, w, 3]
+        Parameters
+        ----------
+        height : int
+            output height, h
+        width : int
+            output width, w
+
+        Returns
+        -------
+        Tensor
+            shape = [h, w, 3]
         """
         raise NotImplementedError()
 
-    def project(self, rays: torch.Tensor, rmat: torch.Tensor):
+    def project(self, rays, rmat):
         """
-        Args:
-            rays            (torch.Tensor)  : shape = [n, h, w, 3]
-            rmat            (torch.Tensor)  : shape = [n, 3, 3]
-        Returns:
-            (torch.Tensor)                  : shape = [n, h, w, 2]
+        Parameters
+        ----------
+        rays : Tensor
+            shape = [n, h, w, 3]
+        rmat : Tensor
+            shape = [n, 3, 3], orthonormal 3D rotation matrix
+
+        Returns
+        -------
+        Tensor
+            shape = [n, h, w, 2]
         """
         raise NotImplementedError()
 
-    def rays(self, rmat: torch.Tensor, height: int = 144, width: int = 256):
+    def rays(self, rmat, height=144, width=256):
         """
-        Args:
-            rmat            (torch.Tensor)  : shape = [n, 3, 3]
-            height          (int)
-            width           (int)
-        Returns:
-            (torch.Tensor)                  : shape = [n, h, w, 3]
+        Parameters
+        ----------
+        rmat : Tensor
+            shape = [n, 3, 3], orthonormal 3D rotation matrix
+        height : int
+            output height, h
+        width : int
+            output width, w
+
+        Returns
+        -------
+        Tensor
+            shape = [n, h, w, 3]
         """
         grid = self.grid(
             height=height,
@@ -45,19 +63,31 @@ class Retina(Module):
 class Angular(Retina):
     def __init__(
         self,
-        degrees: float,
+        degrees,
     ):
+        """
+        Parameters
+        ----------
+        degrees : float
+            maximum visual degrees
+        """
         super().__init__()
         self.degrees = float(degrees)
         self.radians = self.degrees / 180 * torch.pi
 
-    def grid(self, height: int = 144, width: int = 256):
+    def grid(self, height=144, width=256):
         """
-        Args:
-            height          (int)
-            width           (int)
-        Returns:
-            (torch.Tensor)                  : shape = [h, w, 3]
+        Parameters
+        ----------
+        height : int
+            output height, h
+        width : int
+            output width, w
+
+        Returns
+        -------
+        Tensor
+            shape = [h, w, 3]
         """
         x, y = isotropic_grid_2d(
             height=height,
@@ -80,13 +110,19 @@ class Angular(Retina):
         ]
         return torch.stack(grid, dim=2)
 
-    def project(self, rays: torch.Tensor, rmat: torch.Tensor):
+    def project(self, rays, rmat):
         """
-        Args:
-            rays            (torch.Tensor)  : shape = [n, h, w, 3]
-            rmat            (torch.Tensor)  : shape = [n, 3, 3]
-        Returns:
-            (torch.Tensor)                  : shape = [n, h, w, 2]
+        Parameters
+        ----------
+        rays : Tensor
+            shape = [n, h, w, 3]
+        rmat : Tensor
+            shape = [n, 3, 3], orthonormal 3D rotation matrix
+
+        Returns
+        -------
+        Tensor
+            shape = [n, h, w, 2]
         """
         norm = rays.norm(p=2, dim=3, keepdim=True)
         rays = rays / norm
