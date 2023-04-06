93fb00eaf807e45b522c924b0e44decbd83f07a1
Eric Y. Wang <ericwang919@gmail.com>
2023-04-06 11:59:38 -0500

    cortex

--- networks/perspectives.py
+++ networks/perspectives.py
@@ -1,17 +1,25 @@
 import torch
 from functools import reduce
-from typing import Optional, Sequence
 
 from .containers import Module, ModuleList
-from .variables import Stimulus, EyePosition
-from .monitors import Monitor
-from .retinas import Retina
 from .elements import Linear, nonlinearity
 from .utils import isotropic_grid_sample_2d, rmat_3d
 
 
 class Perspective(Module):
-    def __init__(self, stimulus: Stimulus, eye_position: EyePosition):
+    def __init__(
+        self,
+        stimulus,
+        eye_position,
+    ):
+        """
+        Parameters
+        ----------
+        stimulus : .variables.Stimulus
+            stimulus variable
+        eye_position : .variables.EyePosition
+            eye position variable
+        """
         super().__init__()
         self.stimulus = stimulus
         self.eye_position = eye_position
@@ -20,61 +28,73 @@ class Perspective(Module):
     def out_channels(self):
         raise NotImplementedError
 
-    def forward(
-        self,
-        stimulus: torch.Tensor,
-        eye_position: Optional[torch.Tensor] = None,
-        height: int = 144,
-        width: int = 256,
-        pad_mode: str = "constant",
-        pad_value: float = 0,
-    ):
+    def forward(self, stimulus, eye_position=None, height=144, width=256, pad_mode="constant", pad_value=0):
         """
-        Args:
-            stimulus        (torch.Tensor)  : shape = [n, c, h, w]
-            eye_position    (torch.Tensor)  : shape = [n, f]
-            height          (int)           : h'
-            width           (int)           : w'
-            pad_mode        (str)           : 'constant' or 'replicate'
-            pad_value       (float)         : value of padding when pad_mode=='constant'
-        Returns:
-            (torch.Tensor)                  : shape = [n, c', h', w']
+        Parameters
+        ----------
+        stimulus : Tensor
+            shape = [n, c, h, w]
+        eye_position : Tensor
+            shape = [n, f]
+        height : int
+            output height, h'
+        width : int
+            output width, w'
+        pad_mode : str
+            "constant" | "replicate"
+        pad_value : float
+            value of padding when pad_mode=="constant"
+
+        Returns
+        -------
+        Tensor
+            shape = [n, c', h', w']
         """
         raise NotImplementedError()
 
-    def inverse(
-        self,
-        stimulus: torch.Tensor,
-        eye_position: Optional[torch.Tensor] = None,
-        height: int = 144,
-        width: int = 256,
-        pad_mode: str = "constant",
-        pad_value: float = 0,
-    ):
+    def inverse(self, stimulus, eye_position=None, height=144, width=256, pad_mode="constant", pad_value=0):
         """
-        Args:
-            stimulus        (torch.Tensor)  : shape = [n, c, h, w]
-            eye_position    (torch.Tensor)  : shape = [n, f]
-            height          (int)           : h'
-            width           (int)           : w'
-            pad_mode        (str)           : 'constant' or 'replicate'
-            pad_value       (float)         : value of padding when pad_mode=='constant'
-        Returns:
-            (torch.Tensor)                  : shape = [n, c', h', w']
+        Parameters
+        ----------
+        stimulus : Tensor
+            shape = [n, c, h, w]
+        eye_position : Tensor
+            shape = [n, f]
+        height : int
+            output height, h'
+        width : int
+            output width, w'
+        pad_mode : str
+            "constant" | "replicate"
+        pad_value : float
+            value of padding when pad_mode=="constant"
+
+        Returns
+        -------
+        Tensor
+            shape = [n, c', h', w']
         """
         raise NotImplementedError()
 
 
 class MonitorRetina(Perspective):
-    def __init__(
-        self,
-        stimulus: Stimulus,
-        eye_position: EyePosition,
-        monitor: Monitor,
-        retina: Retina,
-        features: Sequence[int],
-        nonlinear: Optional[str] = None,
-    ):
+    def __init__(self, stimulus, eye_position, monitor, retina, features, nonlinear=None):
+        """
+        Parameters
+        ----------
+        stimulus : .variables.Stimulus
+            stimulus variable
+        eye_position : .variables.EyePosition
+            eye position variable
+        monitor : .monitors.Monitor
+            3D monitor model
+        retina : .retinas.Retina
+            3D retina model
+        features : Sequence[int]
+            MLP features
+        nonlinear : str | None
+            nonlinearity
+        """
         super().__init__(stimulus=stimulus, eye_position=eye_position)
         self.monitor = monitor
         self.retina = retina
@@ -93,27 +113,45 @@ class MonitorRetina(Perspective):
     def out_channels(self):
         return self.stimulus.n_channels
 
-    def rmat(self, eye_position: Optional[torch.Tensor] = None):
+    def rmat(self, eye_position=None):
         """
-        Args:
-            eye_position    (torch.Tensor)  : shape = [n, f]
-        Returns:
-            (torch.Tensor)                  : shape = [n, 3, 3]
+        Parameters
+        ----------
+        eye_position : Tensor | None
+            shape = [n, f]
+
+        Returns
+        -------
+        Tensor
+            shape = [n, 3, 3]
         """
         x = self.eye_position(eye_position)
         x = reduce(lambda x, layer: self.nonlinear(layer([x])) * self.gamma, self.layers, x)
         x = self.proj([x])
         return rmat_3d(*x.unbind(1))
 
-    def forward(
-        self,
-        stimulus: torch.Tensor,
-        eye_position: Optional[torch.Tensor] = None,
-        height: int = 144,
-        width: int = 256,
-        pad_mode: str = "constant",
-        pad_value: float = 0,
-    ):
+    def forward(self, stimulus, eye_position=None, height=144, width=256, pad_mode="constant", pad_value=0):
+        """
+        Parameters
+        ----------
+        stimulus : Tensor
+            shape = [n, c, h, w]
+        eye_position : Tensor
+            shape = [n, f]
+        height : int
+            output height, h'
+        width : int
+            output width, w'
+        pad_mode : str
+            "constant" | "replicate"
+        pad_value : float
+            value of padding when pad_mode=="constant"
+
+        Returns
+        -------
+        Tensor
+            shape = [n, c', h', w']
+        """
         rmat = self.rmat(eye_position)
         rays = self.retina.rays(rmat, height, width)
         grid = self.monitor.project(rays)
@@ -125,15 +163,28 @@ class MonitorRetina(Perspective):
         )
         return self.stimulus(out)
 
-    def inverse(
-        self,
-        stimulus: torch.Tensor,
-        eye_position: Optional[torch.Tensor] = None,
-        height: int = 144,
-        width: int = 256,
-        pad_mode: str = "constant",
-        pad_value: float = 0,
-    ):
+    def inverse(self, stimulus, eye_position=None, height=144, width=256, pad_mode="constant", pad_value=0):
+        """
+        Parameters
+        ----------
+        stimulus : Tensor
+            shape = [n, c, h, w]
+        eye_position : Tensor
+            shape = [n, f]
+        height : int
+            output height, h'
+        width : int
+            output width, w'
+        pad_mode : str
+            "constant" | "replicate"
+        pad_value : float
+            value of padding when pad_mode=="constant"
+
+        Returns
+        -------
+        Tensor
+            shape = [n, c', h', w']
+        """
         rays = self.monitor.rays(stimulus.size(0), height, width)
         rmat = self.rmat(eye_position)
         grid = self.retina.project(rays, rmat)
