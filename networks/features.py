c5f22da0053c4a43e5332058d817b0f76d3d032b
Eric Y. Wang <ericwang919@gmail.com>
2023-04-06 19:56:54 -0500

    cortex

--- networks/features.py
+++ networks/features.py
@@ -4,24 +4,24 @@ from .containers import Module
 
 
 class Features(Module):
-    def init(self, units, features):
+    def init(self, units, channels):
         """
         Parameters
         ----------
         units : int
             number of units, u
-        features : int
-            number of features, f
+        channels : int
+            number of channels, c
         """
         raise NotImplementedError()
 
     @property
-    def weights(self):
+    def features(self):
         """
         Returns
         -------
         Tensor
-            shape = [u, f]
+            shape = [u, c]
         """
         raise NotImplementedError()
 
@@ -36,29 +36,28 @@ class Standard(Features):
         """
         super().__init__()
         self.eps = float(eps)
-        self._weights = None
+        self._features = None
 
     def _reset(self):
-        self._weights = None
+        self._features = None
 
-    def init(self, units, features):
+    def init(self, units, channels):
         """
         Parameters
         ----------
         units : int
             number of units, u
-        features : int
-            number of features, f
+        channels : int
+            number of channels, c
         """
-        self.weight = torch.nn.Parameter(torch.ones(units, features))
+        self.weight = torch.nn.Parameter(torch.ones(units, channels))
         self.gain = torch.nn.Parameter(torch.ones(units))
 
-        bound = features**-0.5
+        bound = channels**-0.5
         torch.nn.init.uniform_(self.weight, -bound, bound)
 
     def _param_norm_dims(self):
         yield self.weight, 1
-        yield self.gain, 1
 
     def _param_groups(self, **kwargs):
         if kwargs.get("weight_decay"):
@@ -66,22 +65,22 @@ class Standard(Features):
             yield dict(params=[self.gain], **kwargs)
 
     @property
-    def weights(self):
+    def features(self):
         """
         Returns
         -------
         Tensor
-            shape = [u, f]
+            shape = [u, c]
         """
-        if self._weights is None:
+        if self._features is None:
 
             var, mean = torch.var_mean(self.weight, dim=1, unbiased=False, keepdim=True)
             scale = (var * self.weight.size(dim=1) + self.eps).pow(-0.5)
 
-            self._weights = torch.einsum(
+            self._features = torch.einsum(
                 "U F , U -> U F",
                 (self.weight - mean).mul(scale),
                 self.gain,
             )
 
-        return self._weights
+        return self._features
