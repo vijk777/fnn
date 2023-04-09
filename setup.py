2fa878544fbada22c2bbe4fef4d6345bfced8208
Eric Y. Wang <ericwang919@gmail.com>
2023-04-09 16:03:55 -0500

    setup

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='foundation-cortex',
+    name='foundation-networks',
     version='0',
-    description='Foundation models for the visual cortex',
+    description='Foundation networks of the visual cortex',
     author='Eric Y. Wang',
     author_email='eric.wang2@bcm.edu',
     packages=find_packages(exclude=[]),
