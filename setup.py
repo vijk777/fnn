67dfb05a67a5eac6307227339fc8b881217245f5
Eric Y. Wang <ericwang919@gmail.com>
2023-05-23 11:27:10 -0500

    fnn

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
-from os import path
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import setup, find_packages
 
 setup(
-    name='fnn',
-    version='0',
-    description='Foundation Neural Networks of the Visual Cortex',
-    author='Eric Y. Wang',
-    author_email='eric.wang2@bcm.edu',
+    name="fnn",
+    version="0",
+    description="Foundation Neural Networks of the Visual Cortex",
+    author="Eric Y. Wang",
+    author_email="eric.wang2@bcm.edu",
     packages=find_packages(exclude=[]),
     install_requires=[],
 )
