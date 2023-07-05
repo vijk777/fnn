4e1dbfc567ba645808ae535d002cc6a3e04cbf94
Eric Y. Wang <ericwang919@gmail.com>
2023-07-05 11:07:18 -0500

    fnn

--- fnn/model/recurrents.py
+++ fnn/model/recurrents.py
@@ -290,3 +290,174 @@ class Rvt(Recurrent):
             y = self.proj_y([h], stream=stream)
 
         return y
+
+
+class ConvLstm(Recurrent):
+    """Convolutional Lstm"""
+
+    def __init__(
+        self,
+        recurrent_channels,
+        out_channels,
+        groups=1,
+        kernel_size=3,
+        dropout=0,
+    ):
+        """
+        Parameters
+        ----------
+        recurrent_channels : int
+            recurrent channels per stream
+        out_channels : int
+            out channels per stream
+        groups : int
+            groups per stream
+        kernel_size : int
+            kernel size
+        dropout : float
+            dropout probability -- [0, 1)
+        """
+        if recurrent_channels % groups != 0:
+            raise ValueError("Recurrent channels must be divisible by groups")
+
+        super().__init__()
+
+        self.recurrent_channels = int(recurrent_channels)
+        self.out_channels = int(out_channels)
+        self.groups = int(groups)
+        self.kernel_size = int(kernel_size)
+        self._dropout = float(dropout)
+
+    def _init(self, inputs, streams):
+        """
+        Parameters
+        ----------
+        channels : Sequence[int]
+            [input channels per stream (I), ...]
+        streams : int
+            number of streams, S
+        """
+        self.inputs = list(map(int, inputs))
+        self.streams = int(streams)
+
+        self.drop = StreamDropout(p=self._dropout, streams=self.streams)
+
+        self.proj_x = Conv(
+            channels=self.recurrent_channels,
+            groups=self.groups,
+            streams=self.streams,
+        )
+
+        for _channels in inputs:
+            self.proj_x.add_input(channels=_channels)
+
+        if self.groups > 1:
+            self.proj_x.add_intergroup()
+
+        if self.recurrent_channels == self.out_channels and self.groups == 1:
+            self.proj_y = None
+
+        else:
+            self.proj_y = Conv(
+                channels=self.out_channels,
+                streams=self.streams,
+            )
+            self.proj_y.add_input(
+                channels=self.recurrent_channels,
+            )
+
+        def proj():
+            p = Conv(
+                channels=self.recurrent_channels,
+                groups=self.groups,
+                streams=self.streams,
+            )
+            p.add_input(
+                channels=self.recurrent_channels * 2,
+                groups=self.groups,
+                kernel_size=self.kernel_size,
+            )
+            return p
+
+        self.proj_i = proj()
+        self.proj_f = proj()
+        self.proj_g = proj()
+        self.proj_o = proj()
+
+        self.past = dict()
+
+    def _restart(self):
+        self.dropout(p=self._dropout)
+
+    def _reset(self):
+        self.past.clear()
+
+    @property
+    def channels(self):
+        """
+        Returns
+        -------
+        int
+            output channels per stream (O)
+        """
+        return self.out_channels
+
+    def forward(self, inputs, stream=None):
+        """
+        Parameters
+        ----------
+        inputs : Sequence[Tensor]
+            [[N, S*I, H, W] ...] -- stream is None
+                or
+            [[N, I, H, W] ...] -- stream is int
+        stream : int | None
+            specific stream (int) or all streams (None)
+
+        Returns
+        -------
+        Tensor
+            [N, S*O, H, W] -- stream is None
+                or
+            [N, O, H, W] -- stream is int
+        """
+        if stream is None:
+            channels = self.streams * self.recurrent_channels
+            groups = self.streams * self.groups
+        else:
+            channels = self.recurrent_channels
+            groups = self.groups
+
+        if self.past:
+            assert self.past["stream"] == stream
+            h = self.past["h"]
+            c = self.past["c"]
+        else:
+            self.past["stream"] = stream
+            h = c = torch.zeros(1, channels, 1, 1, device=self.device)
+
+        if self.groups > 1:
+            x = self.proj_x([*inputs, h], stream=stream)
+        else:
+            x = self.proj_x(inputs, stream=stream)
+
+        xh = cat_groups_2d([x, h.expand_as(x)], groups=groups)
+        N, _, H, W = xh.shape
+
+        i = torch.sigmoid(self.proj_i([xh], stream=stream))
+        f = torch.sigmoid(self.proj_f([xh], stream=stream))
+        g = torch.tanh(self.proj_g([xh], stream=stream))
+        o = torch.sigmoid(self.proj_o([xh], stream=stream))
+
+        c = f * c + i * g
+        h = o * torch.tanh(c)
+        h = self.drop(h, stream=stream)
+
+        self.past["c"] = c
+        self.past["h"] = h
+
+        if self.proj_y is None:
+            y = h
+        else:
+            y = self.proj_y([h], stream=stream)
+
+        return y
