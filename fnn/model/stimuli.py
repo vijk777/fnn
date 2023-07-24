6a08527040432ab69460369f2a9c76795678837a
Eric Y. Wang <ericwang919@gmail.com>
2023-07-24 11:42:38 -0500

    stimuli

--- fnn/model/stimuli.py
+++ fnn/model/stimuli.py
@@ -63,7 +63,7 @@ class VisualStimulus(Module):
 class VisualNlm(VisualStimulus):
     """Visual Stimulus with Non-local means Regularization"""
 
-    def __init__(self, bound, init_value=0, spatial_std=1, temporal_std=1, cutoff=4):
+    def __init__(self, bound, init_value=0, init_gain=0.1, spatial_std=1, temporal_std=1, cutoff=4):
         """
         Parameters
         ----------
@@ -71,6 +71,8 @@ class VisualNlm(VisualStimulus):
             stimulus bound
         init_value : float
             initial pixel value
+        init_gain : float
+            initial pixel gain
         spatial_sigma : float
             nlm spatial standard deviation
         temporal_sigma : float
@@ -82,8 +84,11 @@ class VisualNlm(VisualStimulus):
 
         assert bound.vmin == 0
         assert bound.vmax == 1
+
         self.bound = bound
         self.init_value = float(init_value)
+        self.init_gain = float(init_gain)
+
         self.gaussian = Gaussian3d(spatial_std=spatial_std, temporal_std=temporal_std, cutoff=cutoff)
 
     def _init(self, channels, frames, height, width):
@@ -101,9 +106,36 @@ class VisualNlm(VisualStimulus):
         """
         super()._init(channels=channels, frames=frames, height=height, width=width)
 
-        self.frames = Parameter(
+        self.raw = Parameter(
             torch.full([self.channels, self.frames, self.height, self.width], self.init_value),
         )
+
+        numel = self.raw.numel()
+        self.scale = numel**0.5
+
+        self.gain = Parameter(torch.tensor(self.init_gain))
+        self.gain.decay = False
+        self.gain.scale = 1 / numel
+
+        self.bias = Parameter(torch.tensor(0.0))
+        self.bias.decay = False
+        self.bias.scale = 1 / numel
+
+        self._pixels = None
+
+    def _reset(self):
+        self._pixels = None
+
+    @property
+    def pixels(self):
+        if self._pixels is None:
+
+            if self.raw.any():
+                self._pixels = self.raw / self.raw.norm() * self.scale
+            else:
+                self._pixels = self.raw.clone()
+
+        return self._pixels
 
     def forward(self):
         """
@@ -112,7 +144,7 @@ class VisualNlm(VisualStimulus):
         3D Tensor
             [C, H, W] -- stimulus frame
         """
-        yield from self.bound(self.frames).unbind(dim=1)
+        yield from self.bound(self.pixels * self.gain + self.bias).unbind(dim=1)
 
     def penalty(self):
         """https://www.iro.umontreal.ca/~mignotte/IFT6150/Articles/Buades-NonLocal.pdf
@@ -122,7 +154,7 @@ class VisualNlm(VisualStimulus):
         Tensor
             scalar -- penalty value
         """
-        delt = self.frames - self.gaussian(self.frames)
+        delt = self.pixels - self.gaussian(self.pixels)
         return delt.pow(2).sum()
 
     @property
@@ -135,8 +167,40 @@ class VisualNlm(VisualStimulus):
         """
         with torch.no_grad():
 
-            video = self.bound(self.frames)
+            video = self.bound(self.pixels * self.gain + self.bias)
             video = torch.einsum("C F H W -> F H W C", video)
             video = video.mul(255).round().to(device="cpu", dtype=torch.uint8)
 
             return video.numpy()
+
+
+class StaticVisualNlm(VisualNlm):
+    """Static Visual Stimulus with Non-local means Regularization"""
+
+    def __init__(self, bound, init_value=0, spatial_std=1, cutoff=4):
+        """
+        Parameters
+        ----------
+        bound : fnn.model.bounds.Bound
+            stimulus bound
+        init_value : float
+            initial pixel value
+        spatial_sigma : float
+            nlm spatial standard deviation
+        cutoff : float
+            nlm standard deviation cutoff
+        """
+        super().__init__(bound=bound, init_value=init_value, spatial_std=spatial_std, temporal_std=0, cutoff=cutoff)
+
+    @property
+    def pixels(self):
+        if self._pixels is None:
+            if self.raw.any():
+                self._pixels = self.raw / self.raw.norm() * self.scale
+            else:
+                self._pixels = self.raw.clone()
+
+            if self._pixels.requires_grad:
+                self._pixels.register_hook(lambda x: x.mean(1, keepdim=True).expand(-1, self.frames, -1, -1))
+
+        return self._pixels
