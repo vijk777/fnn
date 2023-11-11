4218cb23da3d475ec8358f4b8a8f7f25594d792f
Eric Y. Wang <ericwang919@gmail.com>
2023-11-11 12:51:17 -0600

    mlp monitor retina

--- fnn/model/perspectives.py
+++ fnn/model/perspectives.py
@@ -1,6 +1,6 @@
 from functools import reduce
 from .modules import Module, ModuleList
-from .elements import Linear, FlatDropout, nonlinearity
+from .elements import Linear, FlatDropout, Mlp, nonlinearity
 from .utils import isotropic_grid_sample_2d, rmat_3d
 
 
@@ -177,6 +177,166 @@ class MonitorRetina(Perspective):
         x = self.drop(x)
         x = self.out(x)
         return rmat_3d(*x.unbind(1))
+
+    def forward(self, stimulus, perspective, pad_mode="zeros"):
+        """
+        Parameters
+        ----------
+        stimulus : Tensor
+            [N, S, H, W]
+        perspective : Tensor
+            [N, F]
+        pad_mode : str
+            "zeros" | "replicate"
+
+        Returns
+        -------
+        Tensor
+            [N, P, H', W']
+        """
+        size = max(stimulus.size(0), perspective.size(0))
+
+        rmat = self.rmat(perspective).expand(size, -1, -1)
+        rays = self.retina.rays(rmat)
+        grid = self.monitor.project(rays)
+
+        pixels = self.monitor_pixel(stimulus).expand(size, -1, -1, -1)
+        pixels = isotropic_grid_sample_2d(pixels, grid=grid, pad_mode=pad_mode)
+        pixels = self.retina_pixel(pixels)
+
+        return pixels
+
+    def inverse(self, stimulus, perspective, height=144, width=256, pad_mode="zeros"):
+        """
+        Parameters
+        ----------
+        stimulus : Tensor
+            [N, S, H, W]
+        perspective : Tensor
+            [N, F]
+        height : int
+            output height (H')
+        width : int
+            output width (W')
+        pad_mode : str
+            "zeros" | "replicate"
+
+        Returns
+        -------
+        Tensor
+            [N, P, H', W']
+        """
+        size = max(stimulus.size(0), perspective.size(0))
+
+        rmat = self.rmat(perspective).expand(size, -1, -1)
+        rays = self.monitor.rays(size, height, width)
+        grid = self.retina.project(rays, rmat)
+
+        pixels = self.retina_pixel.inverse(stimulus).expand(size, -1, -1, -1)
+        pixels = isotropic_grid_sample_2d(pixels, grid=grid, pad_mode=pad_mode)
+        pixels = self.monitor_pixel.inverse(pixels)
+
+        return pixels
+
+
+class MlpMonitorRetina(Perspective):
+    """Mlp, Monitor, Retina Perspective"""
+
+    def __init__(
+        self,
+        mlp_features,
+        mlp_layers,
+        mlp_nonlinear,
+        monitor,
+        monitor_pixel,
+        retina,
+        retina_pixel,
+        height,
+        width,
+    ):
+        """
+        Parameters
+        ----------
+        mlp_features : int
+            mlp features
+        mlp_layers : int
+            mlp layers
+        mlp_nonlinear : str
+            mlp nonlinearity
+        monitor : fnn.model.monitors.Monitor
+            3D monitor model
+        monitor_pixel : fnn.model.pixels.Pixel
+            monitor pixel intensity
+        retina : fnn.model.retinas.Retina
+            3D retina model
+        retina_pixel : fnn.model.pixels.Pixel
+            retina pixel intensity
+        height : int
+            retina height
+        width : int
+            retina width
+        """
+        super().__init__()
+
+        self.mlp_features = int(mlp_features)
+        self.mlp_layers = int(mlp_layers)
+        self.mlp_nonlinear = str(mlp_nonlinear)
+
+        self.monitor = monitor
+        self.monitor_pixel = monitor_pixel
+
+        self.retina = retina
+        self.retina_pixel = retina_pixel
+
+        self.height = int(height)
+        self.width = int(width)
+
+    def _init(self, stimuli, perspectives):
+        """
+        Parameters
+        ----------
+        stimuli : int
+            stimulus channels (S)
+        perspectives : int
+            perspective features (F)
+        """
+        self._channels = int(stimuli)
+        self.perspectives = int(perspectives)
+
+        self.retina._init(
+            height=self.height,
+            width=self.width,
+        )
+        self.mlp = Mlp(
+            in_features=self.perspectives,
+            out_features=[self.mlp_features] * self.mlp_layers + [3],
+            out_wnorms=[False] + [True] * self.mlp_layers,
+            out_nonlinears=[self.mlp_nonlinear] * self.mlp_layers + [None],
+        )
+
+    @property
+    def channels(self):
+        """
+        Returns
+        -------
+        int
+            perspective channels (P)
+        """
+        return self._channels
+
+    def rmat(self, perspective):
+        """
+        Parameters
+        ----------
+        perspective : Tensor
+            [N, F]
+
+        Returns
+        -------
+        Tensor
+            [N, 3, 3], 3D rotation matrix
+        """
+        return rmat_3d(*self.mlp(perspective).unbind(1))
 
     def forward(self, stimulus, perspective, pad_mode="zeros"):
         """
