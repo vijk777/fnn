c7ac348f0493dd81d5eb5a0fe837d6975b8f5f35
Eric Y. Wang <ericwang919@gmail.com>
2024-02-04 04:26:48 -0600

    mse loss

--- fnn/model/units.py
+++ fnn/model/units.py
@@ -95,3 +95,47 @@ class Poisson(Unit):
         """
         r = readout.squeeze(2)
         return r.exp() - r * unit
+
+
+class MSE(Unit):
+    """MSE Unit"""
+
+    @property
+    def readouts(self):
+        """
+        Returns
+        -------
+        int
+            readouts per unit (R)
+        """
+        return 1
+
+    def forward(self, readout):
+        """
+        Parameters
+        ----------
+        readout : Tensor
+            [N, U, R]
+
+        Returns
+        -------
+        Tensor
+            [N, U]
+        """
+        return readout.squeeze(2).exp()
+
+    def loss(self, readout, unit):
+        """
+        Parameters
+        ----------
+        readout : Tensor
+            [N, U, R]
+        unit : Tensor
+            [N, U]
+
+        Returns
+        -------
+        Tensor
+            [N, U]
+        """
+        return (readout.squeeze(2) - unit).pow(2)
