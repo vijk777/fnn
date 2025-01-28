91648306ccd1edd991152815d8532fe2862ec86b
Eric Y. Wang <ericwang919@gmail.com>
2025-01-28 12:29:31 -0600

    microns

--- fnn/microns/__init__.py
+++ fnn/microns/__init__.py
@@ -1,4 +1,55 @@
-from fnn.microns.load import params, units, unit_ids
+import os
+import tempfile
+import logging
+from fnn.microns.load import download, unzip, params, units, unit_ids
 from fnn.microns.build import network
 
+URL = "https://www.dropbox.com/scl/fi/40rbdh38ylw0aui6pfv8j/microns.zip?rlkey=jozm6cgqpp5dafcqedsuffl8h&st=8xd25u6y&dl=1"
 MD5 = "58fcac4b31ad2902c81e339432cec787"
+
+logger = logging.getLogger("fnn.microns")
+logger.addHandler(logging.StreamHandler())
+
+
+def scan(session, scan_idx, directory=None, verbose=True):
+    """
+    Parameters
+    ----------
+    session : int
+        scan session
+    scan_idx : int
+        scan index
+    directory : os.PathLike | None
+        directory for model parameters and metadata. defaults to current working directory
+    verbose : bool
+        display data download progress
+
+    Returns
+    -------
+    fnn.model.networks.Visual
+        predictive model of the experimental scan
+    pd.DataFrame
+        dataframe mapping readout ids to unit ids
+    """
+    logger.setLevel(logging.INFO if verbose else logging.WARNING)
+
+    directory = os.path.join(directory or os.getcwd(), "fnn", "microns")
+    load = lambda f: f(session, scan_idx, directory)
+
+    if not os.path.exists(directory):
+        logger.info(f"Downloading model parameters and metadata to `{directory}`")
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            zipfile = os.path.join(tmpdir, "microns.zip")
+
+            md5 = download(URL, zipfile, verbose=verbose)
+            assert md5 == MD5, f"md5 for downloaded file is {md5}, expected {MD5}"
+
+            unzip(zipfile, directory)
+
+    model = network(load(units))
+    model.load_state_dict(load(params))
+
+    metadata = load(unit_ids)
+
+    return model, metadata
--- fnn/microns/load.py
+++ fnn/microns/load.py
@@ -1,23 +1,10 @@
 import os
 import hashlib
 import zipfile
+import requests
 import torch
 import pandas as pd
-
-
-def directory(root=None):
-    """
-    Parameters
-    ----------
-    root : os.PathLike
-        root directory
-
-    Returns
-    -------
-    str
-        microns data directory
-    """
-    return os.path.join(root or os.getcwd(), "microns")
+from tqdm import tqdm
 
 
 def md5sum(file_path, chunk_size=8192):
@@ -43,20 +30,49 @@ def md5sum(file_path, chunk_size=8192):
     return md5.hexdigest()
 
 
-def unzip(file_path, target=None):
+def download(url, file_path, chunk_size=8192, verbose=True):
+    """
+    Parameters
+    ----------
+    url : str
+        source url
+    file_path : os.PathLike
+        destination file path
+    chunk_size : int
+        size of the chunks (in bytes) to read
+
+    Returns
+    -------
+    str
+        MD5 checksum of downloaded file as a hexadecimal string
+    """
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    size = int(response.headers.get("content-length", 0))
+
+    with open(file_path, "wb") as file:
+        with tqdm(total=size, unit="B", unit_scale=True, disable=not verbose) as bar:
+            for chunk in response.iter_content(chunk_size):
+                file.write(chunk)
+                bar.update(len(chunk))
+
+    return md5sum(file_path, chunk_size)
+
+
+def unzip(file_path, directory=None):
     """
     Parameters
     ----------
     file_path : os.PathLike
         path to the zip file
-    target : os.PathLike
-        target root directory
+    directory : os.PathLike
+        destination directory
     """
     with zipfile.ZipFile(file_path, "r") as zf:
-        zf.extractall(directory(target))
+        zf.extractall(directory)
 
 
-def params(session, scan_idx, root=None):
+def params(session, scan_idx, directory):
     """
     Parameters
     ----------
@@ -64,24 +80,22 @@ def params(session, scan_idx, root=None):
         scan session
     scan_idx : int
         scan index
-    root : os.PathLike
-        root directory
+    directory : os.PathLike
+        data directory
 
     Returns
     -------
     Dict[str, torch.Tensor]
         model parameters
     """
-    microns_dir = directory(root)
 
     def load(path):
-
-        return torch.load(os.path.join(microns_dir, path), map_location="cpu")
+        return torch.load(os.path.join(directory, path), map_location="cpu")
 
     return dict(**load("params_core.pt"), **load(f"params_{session}_{scan_idx}.pt"))
 
 
-def units(session, scan_idx, root=None):
+def units(session, scan_idx, directory):
     """
     Parameters
     ----------
@@ -89,15 +103,15 @@ def units(session, scan_idx, root=None):
         scan session
     scan_idx : int
         scan index
-    root : os.PathLike
-        root directory
+    directory : os.PathLike
+        data directory
 
     Returns
     -------
     int
         number of units
     """
-    path = os.path.join(directory(root), "scans.csv")
+    path = os.path.join(directory, "scans.csv")
     df = pd.read_csv(path).set_index(["session", "scan_idx"])
     try:
         return int(df.loc[session, scan_idx].units)
@@ -105,7 +119,7 @@ def units(session, scan_idx, root=None):
         raise ValueError(f"Scan {session}-{scan_idx} not found.")
 
 
-def unit_ids(session, scan_idx, root=None):
+def unit_ids(session, scan_idx, directory):
     """
     Parameters
     ----------
@@ -113,15 +127,15 @@ def unit_ids(session, scan_idx, root=None):
         scan session
     scan_idx : int
         scan index
-    root : os.PathLike
-        root directory
+    directory : os.PathLike
+        data directory
 
     Returns
     -------
     pd.DataFrame
         dataframe mapping readout ids to unit ids
     """
-    path = os.path.join(directory(root), "units.csv")
+    path = os.path.join(directory, "units.csv")
     df = pd.read_csv(path).groupby(["session", "scan_idx"])
     try:
         return df.get_group((session, scan_idx)).set_index("readout_id")
